c630c80be7c0590477a52bbdabf88a6dff55ddf7
André Jaenisch <andre.jaenisch@posteo.de>
2018-05-15 00:22:24 +0200

    Added typechecking using flow-jsdoc.

--- src/ui.js
+++ src/ui.js
@@ -1,4 +1,17 @@
+/* @flow */
+
+/**
+ * Class representing interacting with a UI.
+ * @class UI
+ */
 export default class UI {
+    /**
+     * Aborts the execution and informs the user.
+     * @method UI#abort
+     * @access public
+     *
+     * @param { string } text - Text to display
+     */
     abort (text) {
         window.alert(text);
     }
