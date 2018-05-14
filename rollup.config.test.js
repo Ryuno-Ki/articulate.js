2e7804aabe2cb68d3a909d56d525dc31f7d20410
André Jaenisch <andre.jaenisch@posteo.de>
2018-05-14 19:54:02 +0200

    List all HTML elements in JSON files and make Rollup understand it.

--- rollup.config.test.js
+++ rollup.config.test.js
@@ -1,11 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
+const json = require('rollup-plugin-json');
 const { promisify } = require('es6-promisify');
 
 const readdir = promisify(fs.readdir);
 const sourceDirectory = path.join(__dirname, 'src');
 const testDirectory = path.join(__dirname, 'test');
+
+function isRegularFile (file) {
+    return !file.startsWith('.') && file.endsWith('.js');
+}
 
 function configFromFile (file) {
     return Promise.resolve({
@@ -13,7 +18,10 @@ function configFromFile (file) {
         output: {
             file: path.join(testDirectory, file),
             format: 'cjs'
-        }
+        },
+        plugins: [
+            json()
+        ]
     });
 }
 
@@ -21,8 +29,8 @@ export default new Promise(async function (resolve, reject) {
     try {
         const files = await readdir(sourceDirectory);
         const jsFiles = files.filter((file) => {
-            return !file.startsWith('.') && file.endsWith('.js')
-        });
+            return isRegularFile(file);
+        })
         return resolve(Promise.all(jsFiles.map(configFromFile)));
     } catch (exception) {
         return reject(exception);
--- src/pronounciation-mapping.js
+++ src/pronounciation-mapping.js
@@ -1,11 +1,36 @@
-import VoiceTag from './voice-tag';
+import * as htmlContentElements from './html.content.json';
+import * as htmlEditElements from './html.edit.json';
+import * as htmlEmbeddedElements from './html.embedded.json';
+import * as htmlFormsElements from './html.forms.json';
+import * as htmlInlineElements from './html.inline.json';
+import * as htmlInteractiveElements from './html.interactive.json';
+import * as htmlMultimediaElements from './html.multimedia.json';
+import * as htmlScriptsElements from './html.scripts.json';
+import * as htmlTableElements from './html.table.json';
+import * as htmlTextElements from './html.text.json';
+
+const allElements = [
+  ...htmlContentElements.elements,
+  ...htmlEditElements.elements,
+  ...htmlEmbeddedElements.elements,
+  ...htmlFormsElements.elements,
+  ...htmlInlineElements.elements,
+  ...htmlInteractiveElements.elements,
+  ...htmlMultimediaElements.elements,
+  ...htmlScriptsElements.elements,
+  ...htmlTableElements.elements,
+  ...htmlTextElements.elements,
+]
+
+console.log(allElements);
 
 // Map HTML tags to text to be spoken before or after
 // TODO: Look at https://github.com/GNOME/orca/blob/master/src/orca/messages.py
 // or https://github.com/nvaccess/nvda/blob/master/source/controlTypes.py#L198
-export default {
+export default allElements;
+/*
     "q": new VoiceTag("quote,", ", unquote,"),
     "ol": new VoiceTag("Start of list.", "End of list."),
     "ul": new VoiceTag("Start of list.", "End of list."),
     "blockquote": new VoiceTag("Blockquote start.", "Blockquote end."),
-}
+*/
