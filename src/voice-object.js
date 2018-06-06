7e8f4eae113d5afc7d9e54353027d477e6071064
André Jaenisch <andre.jaenisch@posteo.de>
2018-06-06 23:02:18 +0200

    Fixed flow check. Added typings.

--- src/voice-object.js
+++ src/voice-object.js
@@ -1,4 +1,14 @@
+/* @flow */
+
+/**
+ * Class representing an object to speak.
+ * @class VoiceObject
+ */
 export default class VoiceObject {
+    /**
+     * @param { string } name - Name of the object.
+     * @param { string } language - In which language shall it be spoken?
+     */
     constructor(options) {
         const { name, language } = options;
         this.name = name;
--- src/voice-options.js
+++ src/voice-options.js
@@ -1,3 +1,12 @@
+/* @flow */
+
+/**
+ * Holding default config for TTS
+ * @name VoiceOptions
+ * @property { number } rate - How fast to speak
+ * @property { number } pitch - How high to speak
+ * @property { number } volume - How loud to speak
+ */
 export default {
     rate: 1.10,
     pitch: 1,
